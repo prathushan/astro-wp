0804c238bb7228593840fe913674dc28b19b0af4
np-usha <prathusha.n22@gmail.com>
2025-02-14 18:17:38 +0530

    posts to pages shift

--- src/lib/wp.js
+++ src/lib/wp.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-export async function getPosts() {
+export async function getPages() {
   try {
-    const res = await axios.get('https://dev-fit-quest-challenge.pantheonsite.io/wp-json/wp/v2/posts');
+    const res = await axios.get('https://dev-fit-quest-challenge.pantheonsite.io/wp-json/wp/v2/pages');
     return res.data;
   } catch (error) {
-    console.error('Error fetching posts:', error);
+    console.error('Error fetching pages:', error);
     return [];
   }
 }
