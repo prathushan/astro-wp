1f45e2821b46e21ddffa1a97a7bc0cfb3a7a62c2
np-usha <prathusha.n22@gmail.com>
2025-02-14 18:23:48 +0530

    posts to pages shift

--- src/lib/wp.js
+++ src/lib/wp.js
@@ -1,11 +1,14 @@
 import axios from 'axios';
 
-export async function getPages() {
+export async function getHomePage() {
   try {
-    const res = await axios.get('https://dev-fit-quest-challenge.pantheonsite.io/wp-json/wp/v2/pages');
-    return res.data;
+    const res = await axios.get('https://dev-fit-quest-challenge.pantheonsite.io/wp-json/wp/v2/pages?slug=home');
+    if (res.data.length > 0) {
+      return res.data[0]; // Get the first matching page (home)
+    }
+    return null;
   } catch (error) {
-    console.error('Error fetching pages:', error);
-    return [];
+    console.error('Error fetching homepage:', error);
+    return null;
   }
 }
