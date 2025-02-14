f05c1bcb297ca3a457e284e26fd5c01f2f343c8b
np-usha <prathusha.n22@gmail.com>
2025-02-14 17:36:06 +0530

    Fix: Added Vercel adapter for SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/server';
 
 export default defineConfig({
-  output: 'server', // Enables SSR (Server-Side Rendering)
+  output: 'server', // Enable SSR
+  adapter: vercel(),
   site: 'https://dev-fit-quest-challenge.pantheonsite.io/'
 });
